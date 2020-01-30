abbddd99c97dafd678c2423590680074dc403f41
akhil850 <akhil850@gmail.com>
2020-01-30 19:03:08 +0530

    Linux Basic Security Setup v1.2

--- child_scripts/csf_setup.sh
+++ child_scripts/csf_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 yum install wget -y
-yum install perl-libwww-perl.noarch perl-LWP-Protocol-https.noarch perl-GDGraph -y
+yum install perl-libwww-perl.noarch perl-LWP-Protocol-https.noarch perl-GDGraph -y 2>&1 >/dev/null
 cd /usr/src
 rm -rf csf.tgz
 wget https://download.configserver.com/csf.tgz
@@ -14,12 +14,14 @@ echo "Testing CSF Compatibility"
 perl /usr/local/csf/bin/csftest.pl 2>&1 >/dev/null
 echo "Done"
 echo "Enabling CSF and LFD"
-sed -i 's/TESTING = "1"/TESTING = "0"/g' csf.conf
+sed -i 's/TESTING = "1"/TESTING = "0"/g' /etc/csf/csf.conf
 systemctl start csf 2>&1 >/dev/null
 systemctl start lfd 2>&1 >/dev/null
 systemctl enable csf 2>&1 >/dev/null
 systemctl enable lfd 2>&1 >/dev/null
-echo "SSH Service Status::"
-service lfd status |awk '{print $2,$3}' | grep active --color
-service csf status |awk '{print $2,$3}' | grep active --color
-/usr/sbin/csf -r
+echo "Enabling CSF..."
+/usr/sbin/csf -e 2>&1 >/dev/null
+echo "Flushing CSF..."
+/usr/sbin/csf -f 2>&1 >/dev/null
+echo "Reloading CSF..."
+/usr/sbin/csf -r 2>&1 >/dev/null
