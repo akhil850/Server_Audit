4a0803724cb2e0c1e56ec714f13aabef49909df4
akhil850 <akhil850@gmail.com>
2020-01-30 15:44:36 +0530

    Commint #2

--- main.sh
+++ main.sh
@@ -8,22 +8,32 @@ echo "Time : \n$DATE \n$TIME"
 echo
 echo "#######################################"
 echo
-echo "Peforming Full System Update"
+echo "1. Peforming Full System Update"
 echo "------------------------------------"
 sh ./scripts/update_system.sh
 echo "System Fully Updated"
 echo
-echo "Disabling SELINUX"
+echo "2. Disabling SELINUX"
 echo "------------------------------------"
 sh ./scripts/selinux_disable.sh
 echo "Disabled"
 echo
-echo "CSF will be installed"
+echo "2. CSF will be installed"
 echo "------------------------------------"
 sh ./scripts/csf_setup.sh
 echo "CSF Installed"
 echo
-echo "Rkhunter Will be installed"
+echo "3. Rkhunter Will be installed"
 echo "------------------------------------"
 sh ./scripts/rkhunter_setup.sh
 echo "Rkhunter Installed"
+echo
+echo "4. Changing SSH port"
+echo "------------------------------------"
+sh ./scripts/ssh_port_change.sh
+echo "Rkhunter Installed"
+echo
+echo "4. Adding Admin IPs"
+echo "------------------------------------"
+sh ./scripts/allow_ips.sh
+echo "Added important IPs"
---
+++ scripts/allow_ips.sh
@@ -0,0 +1,2 @@
+for i in `curl https://raw.githubusercontent.com/akhil850/Server_Audit/master/important_ips`; do echo $i >> /etc/csf/csf.ignore; done
+for i in `curl https://raw.githubusercontent.com/akhil850/Server_Audit/master/important_ips`; do csf -a $i; done
