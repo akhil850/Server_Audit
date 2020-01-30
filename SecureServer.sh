284b7e59db40888ff443b0200b01f050d21ea622
akhil850 <akhil850@gmail.com>
2020-01-30 16:27:41 +0530

    Version 1.0

--- SecureServer.sh
+++ SecureServer.sh
@@ -1,8 +1,37 @@
 #!/usr/bin/env bash
+DATE=$(date +"%m-%d-%Y")
+TIME=$(date +"%r")
+echo "#######################################"
+echo ""
+echo "Linux Basic Security Audit"
+echo "Starting Time : \n$DATE \n$TIME"
+echo ""
+echo "#######################################"
+echo ""
+clear
 echo "Installing Git...."
-sudo yum install epel-release -y 2>&1 >/dev/null
-sudo yum install git -y 2>&1 >/dev/null
+sudo yum install epel-release curl git -y 2>&1 >/dev/null
 echo "Done"
 echo "Setting Up Working Directories [/usr/src/] "
 cd /usr/src/
-git clone https://github.com/akhil850/Server_Audit.git
+git clone https://github.com/akhil850/Server_Audit.git  2>&1 >/dev/null
+cd Server_Audit/
+while true; do
+    read -p "Do you wish to install this program?" yn
+    case $yn in
+        [Yy]* ) sh main.sh; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo "Cleaning Up Working Directories [/usr/src/] "
+rm -rf /usr/src/Server_Audit/ 
+echo "#######################################"
+echo "#######################################"
+echo
+echo "!!!!! Completed !!!!!"
+echo "Linux Basic Security Setups"
+echo "Completed Time : \n$DATE \n$TIME"
+echo
+echo "#######################################"
+echo "#######################################"
--- main.sh
+++ main.sh
@@ -1,55 +1,37 @@
 #!/usr/bin/env bash
-DATE=$(date +"%m-%d-%Y")
-TIME=$(date +"%r")
-echo "#######################################"
-echo ""
-echo "Linux Basic Security Audit"
-echo "Starting Time : \n$DATE \n$TIME"
-echo ""
-echo "#######################################"
-echo ""
-clear
 echo "1. Peforming Full System Update"
 echo "------------------------------------"
 sh ./child_scripts/update_system.sh
 echo "System Fully Updated"
 echo ""
-clear
+echo ""
 echo "2. Disabling SELINUX"
 echo "------------------------------------"
 sh ./child_scripts/selinux_disable.sh
 echo "Disabled"
 echo ""
-clear
+echo ""
 echo "3. CSF will be installed"
 echo "------------------------------------"
 sh ./child_scripts/csf_setup.sh
 echo "CSF Installed"
 echo ""
-clear
+echo ""
 echo "3. Rkhunter Will be installed"
 echo "------------------------------------"
 sh ./child_scripts/rkhunter_setup.sh
 echo "Rkhunter Installed"
+echo ""
 echo ""
 echo "4. Changing SSH port"
 echo "------------------------------------"
 sh ./child_scripts/ssh_port_change.sh
 echo "Rkhunter Installed"
 echo ""
-clear
+echo ""
 echo "4. Adding Admin IPs"
 echo "------------------------------------"
 sh ./child_scripts/allow_ips.sh
 echo "Added important IPs"
 echo ""
-clear
-echo "#######################################"
-echo "#######################################"
-echo
-echo "!!!!! Completed !!!!!"
-echo "Linux Basic Security Setups"
-echo "Completed Time : \n$DATE \n$TIME"
-echo
-echo "#######################################"
-echo "#######################################"
+echo ""
