0b0bb3a23943c4e0ee029dd3cdb8f2773b32ae2a
akhil850 <akhil850@gmail.com>
2020-01-30 17:58:29 +0530

    Bug #!

--- SecureServer.sh
+++ SecureServer.sh
@@ -9,31 +9,19 @@ echo ""
 echo "#######################################"
 echo ""
 echo "Installing Required Modules...."
-sudo yum install epel-release curl git -y 2>&1 >/dev/null
+sudo yum install epel-release curl git -y
 echo "Done"
 echo "Setting Up Working Directories [/usr/src/] "
 cd /usr/src/
 git clone https://github.com/akhil850/Server_Audit.git  2>&1 >/dev/null
 cd Server_Audit/
 echo "User confirmaton Required"
-#Logic to take user input from shell
-
-while true
-do
- read -r -p "Are You Sure? [Y/n] " input
-
- case $input in
-     [yY][eE][sS]|[yY])
- echo "Yes"
- ;;
-     [nN][oO]|[nN])
- echo "No"
-        ;;
-     *)
- echo "Invalid input..."
- ;;
- esac
-done
+read -p "Continue (y/n)?" choice
+case "$choice" in
+  y|Y ) echo "yes";;
+  n|N ) echo "no";;
+  * ) echo "invalid";;
+esac
 echo "Cleaning Up Working Directories [/usr/src/] "
 rm -rf /usr/src/Server_Audit/
 echo "#######################################"
