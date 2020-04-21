94fdb1f7daa21eecbc94b3d00795fe43d940a1a3
Akhil <akhil850@gmail.com>
2020-04-21 19:58:24 +0530

    Error correction

--- SecureServer.sh
+++ SecureServer.sh
@@ -9,25 +9,25 @@ echo "Starting Time : $DATE $TIME"
 echo ""
 echo "#######################################"
 echo ""
-echo "Running Full System Update"
-sh ./child_scripts/update_system.sh
 echo "Setting Up Working Directories"
 cd /usr/src/
 rm -rf Server_Audit
 git clone -q https://github.com/akhil850/Server_Audit.git  2>&1 >/dev/null
 chmod +x /usr/src/Server_Audit/ -R
+echo "Running Full System Update"
+sh ./child_scripts/update_system.sh
 #Main Script starts
 sh main.sh
-echo "Cleaning Up Working Directories..Please Wait"
+echo "Cleaning Up...Please Wait"
 rm -rf /usr/src/Server_Audit/
-echo "Cleaning Complete"
 clear
 echo "#######################################"
 echo "#######################################"
 echo
 echo "!!!!! Completed !!!!!"
+echo ""
 echo "Linux Basic Security Setup v1.2"
 echo "Completed Time : $DATE $TIME"
-echo
+echo ""
 echo "#######################################"
 echo "#######################################"
