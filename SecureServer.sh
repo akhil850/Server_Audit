26e57bffb12c3972e515ca63b5aa7d6ee8d0a3f9
Akhil <akhil850@gmail.com>
2020-04-21 20:32:36 +0530

    Corr

--- SecureServer.sh
+++ SecureServer.sh
@@ -15,7 +15,8 @@ git clone -q https://github.com/akhil850/Server_Audit.git  2>&1 >/dev/null
 chmod +x /usr/src/Server_Audit/ -R
 sh /usr/src/Server_Audit/child_scripts/update_system.sh
 #Main Script starts
-sh /usr/src/Server_Audit/main.sh
+cd /usr/src/Server_Audit/
+sh main.sh
 echo "Cleaning Up...Please Wait"
 rm -rf /usr/src/Server_Audit/
 clear
