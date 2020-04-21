b16e79734e48b60bdec6b0d5a2d9a92bc163be3f
Akhil <akhil850@gmail.com>
2020-04-21 20:29:33 +0530

    Error

--- child_scripts/update_system.sh
+++ child_scripts/update_system.sh
@@ -4,3 +4,4 @@ echo "Running Full System Update"
 # sudo yum install epel-release curl git wget -y 2>&1 >/dev/null
 # yum update -y 2>&1 >/dev/null
 sh /usr/src/Server_Audit/child_scripts/loader.sh
+cd /usr/src/Server_Audit/
