6a00e1e234897c8b168fb04703f7680d5d13662e
Akhil <akhil850@gmail.com>
2020-04-21 20:17:19 +0530

    Error test

--- SecureServer.sh
+++ SecureServer.sh
@@ -13,8 +13,7 @@ cd /usr/src/
 rm -rf Server_Audit
 git clone -q https://github.com/akhil850/Server_Audit.git  2>&1 >/dev/null
 chmod +x /usr/src/Server_Audit/ -R
-cd /usr/src/Server_Audit/
-sh ./child_scripts/update_system.sh
+sh /usr/src/Server_Audit/child_scripts/update_system.sh
 clear
 #Main Script starts
 sh main.sh
--- main.sh
+++ main.sh
@@ -2,26 +2,26 @@
 #Linux Basic Security Setup v1.2
 echo "1. Disabling SELINUX"
 echo "------------------------------------"
-sh ./child_scripts/selinux_disable.sh
+sh /usr/src/Server_Audit/child_scripts/selinux_disable.sh
 echo ""
 echo ""
 echo "2. CSF will be installed"
 echo "------------------------------------"
-sh ./child_scripts/csf_setup.sh
+sh /usr/src/Server_Audit/child_scripts/csf_setup.sh
 echo ""
 echo ""
 echo "3. Rkhunter will be installed"
 echo "------------------------------------"
-sh ./child_scripts/rkhunter_setup.sh
+sh /usr/src/Server_Audit/child_scripts/rkhunter_setup.sh
 echo ""
 echo ""
 echo "4. SSH Port will be changed to 33122"
 echo "------------------------------------"
-sh ./child_scripts/ssh_port_change.sh
+sh /usr/src/Server_Audit/child_scripts/ssh_port_change.sh
 echo ""
 echo ""
 echo "5. Adding Admin IPs"
 echo "------------------------------------"
-sh ./child_scripts/allow_ips.sh
+sh /usr/src/Server_Audit/child_scripts/allow_ips.sh
 echo ""
 echo ""
