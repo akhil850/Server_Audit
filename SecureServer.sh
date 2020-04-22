81d209be7679239133cc55a8a7cf39927e8b6447
Akhil <akhil850@gmail.com>
2020-04-22 11:08:56 +0530

    Minor fixes

--- SecureServer.sh
+++ SecureServer.sh
@@ -31,7 +31,7 @@ echo $'\e[43m'#######################################$white
 echo ""
 echo -e "\e[1;36mLinux Basic Security Setup v1.3 \e[0m"
 echo ""
-echo -e "\e[1;32m ** Completed ** \e[0m
+echo -e "\e[1;32m ** Completed ** \e[0m"
 echo ""
 echo "Completed Time : $DATE $TIME"
 echo ""
