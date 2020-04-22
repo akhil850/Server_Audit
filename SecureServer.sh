88eb963aef7375e1ae94b726c6a91f7da939b404
Akhil <akhil850@gmail.com>
2020-04-22 11:32:12 +0530

    Testing

--- SecureServer.sh
+++ SecureServer.sh
@@ -3,6 +3,7 @@
 DATE=$(date +"%m-%d-%Y")
 TIME=$(date +"%r")
 white=$'\e[0m'
+echo ""
 echo $'\e[1;41m'#######################################$'\e[0m'
 echo ""
 echo -e "\e[1;36mLinux Basic Security Setup v1.2 \e[0m"
