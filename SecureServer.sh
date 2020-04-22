7220048eceb5bd06586d80318e1a105da730c984
Akhil <akhil850@gmail.com>
2020-04-22 11:18:44 +0530

    Finalised testing

--- SecureServer.sh
+++ SecureServer.sh
@@ -2,18 +2,12 @@
 #Linux Basic Security Setup v1.2
 DATE=$(date +"%m-%d-%Y")
 TIME=$(date +"%r")
-red=$'\e[1;31m'
-grn=$'\e[1;32m'
-blu=$'\e[1;34m'
-mag=$'\e[1;35m'
-cyn=$'\e[1;36m'
-white=$'\e[0m'
-echo $'\e[43m'#######################################$white
+echo -e "\e[1;36m#######################################\e[0m"
 echo ""
 echo -e "\e[1;36mLinux Basic Security Setup v1.2 \e[0m"
 echo "Starting Time : $DATE $TIME"
 echo ""
-echo $'\e[43m'#######################################$white
+echo -e "\e[1;36m#######################################\e[0m"
 echo ""
 cd /usr/src/
 rm -rf Server_Audit
