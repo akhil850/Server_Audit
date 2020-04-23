20edffc900f5354cd4fa5883098de303cf87c481
Akhil <akhil850@gmail.com>
2020-04-23 12:11:05 +0530

    Reordered chain

--- main.sh
+++ main.sh
@@ -13,9 +13,9 @@ echo "------------------------------------"
 sh /usr/src/Server_Audit/child_scripts/selinux_disable.sh
 echo ""
 echo ""
-echo -e "\e[1;33m3. CSF Installation\e[0m"
+echo -e "\e[1;33m5. SSH Port change to$red 33122\e[0m"
 echo "------------------------------------"
-sh /usr/src/Server_Audit/child_scripts/csf_setup.sh
+sh /usr/src/Server_Audit/child_scripts/ssh_port_change.sh
 echo ""
 echo ""
 echo -e "\e[1;33m4. Rkhunter Installation\e[0m"
@@ -23,9 +23,9 @@ echo "------------------------------------"
 sh /usr/src/Server_Audit/child_scripts/rkhunter_setup.sh
 echo ""
 echo ""
-echo -e "\e[1;33m5. SSH Port change to$red 33122\e[0m"
+echo -e "\e[1;33m3. CSF Installation\e[0m"
 echo "------------------------------------"
-sh /usr/src/Server_Audit/child_scripts/ssh_port_change.sh
+sh /usr/src/Server_Audit/child_scripts/csf_setup.sh
 echo ""
 echo ""
 echo "6. Adding Admin IPs"
