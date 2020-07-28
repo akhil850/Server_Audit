91f1f00874e5f2b256d528f0447cb51a2a8e15ac
akhil850 <akhil850@gmail.com>
2020-07-29 04:35:26 +0530

    Updates

--- child_scripts/rkhunter_setup.sh
+++ child_scripts/rkhunter_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #Linux Basic Security Setup v1.3
 sudo yum install rkhunter -y 2>&1 >/dev/null
-sudo rkhunter --update -y 2>&1 >/dev/null
+sudo rkhunter --update 2>&1 >/dev/null
 sudo rkhunter --propupd 2>&1 >/dev/null
 sudo rkhunter --check --sk 2>&1 >/dev/null
 echo -e "\e[1;35mDone \e[0m"
