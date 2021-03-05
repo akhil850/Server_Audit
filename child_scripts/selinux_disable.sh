b4e6fe98bbc0c263ee0e28f29d287aa0154b61cb
akhil850 <akhil850@gmail.com>
2021-03-05 10:19:30 +0530

    Updates

--- child_scripts/selinux_disable.sh
+++ child_scripts/selinux_disable.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Linux Basic Security Setup v1.3
 setenforce 0  2>&1 >/dev/null
-sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 sudo sestatus
 echo -e "\e[1;35mDone \e[0m"
